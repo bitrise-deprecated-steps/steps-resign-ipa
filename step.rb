4d48a5fd7ed8b37a21a28ca33a310dd5c630f8f6
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-08-24 16:23:23 +0200

    log configs

--- step.rb
+++ step.rb
@@ -2,17 +2,25 @@ require "#{__dir__}/resigner"
 
 begin
   distribution_type = ENV['distribution_type']
+  ipa_path = ENV['ipa_path']
+  team_id = ENV['itunes_connect_team_id']
+  app_id_prefix = ENV['app_id_prefix']
+
+  puts 'Configs:'
+  puts "  * ipa_path: #{ipa_path}"
+  puts "  * distribution_type: #{distribution_type}"
+  puts "  * team_id: #{team_id}"
+  puts "  * app_id_prefix: #{app_id_prefix}"
+  puts
+
   raise 'distribution_type is not set' unless distribution_type
   raise 'invalid distribution type' unless %w(appstore inhouse development).include?(distribution_type.downcase)
 
-  ipa_path = ENV['ipa_path']
   raise 'ipa_path is not set' unless ipa_path
   raise 'File not found at ipa_path' unless File.exist?(ipa_path)
 
-  team_id = ENV['itunes_connect_team_id']
   team_id = nil if ENV['itunes_connect_team_id'].to_s.eql?('')
 
-  app_id_prefix = ENV['app_id_prefix']
   app_id_prefix = nil if ENV['app_id_prefix'].to_s.eql?('')
 
   resigner = Resigner.new
